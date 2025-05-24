1509010d871e4acf8b8785f010a04ad6fd0aa4e6
Your Name <vigneshhfine07@gmail.com>
2025-05-24 23:17:05 +0530

    our principle features done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/heroSection/Hero';
 import AboutUs from './components/aaboutUs/AboutUs';
+import Principalreview from './components/principalreview/Principalreview';
+// import Principalreview from './components/principalreview/Principalreview';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar />
       <Hero />
       <AboutUs />
+       */}
+       < Principalreview />
     </div>
   )
 }
