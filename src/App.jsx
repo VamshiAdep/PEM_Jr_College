655be4232c5146a05150b744ddc041c6bbfd196c
Laxman8261 <chiduralalaxman@gmail.com>
2025-05-25 12:49:27 +0530

    Your changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar';
-import Hero from './components/heroSection/Hero';
 import AboutUs from './components/aaboutUs/AboutUs';
+import OurManagement from './components/OurManagment/OurManagment';
+import Footer from './components/footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Hero />
-      <AboutUs />
-    </div>
-  )
-}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/management" element={<OurManagement />} />
+        {/* Add more routes here */}
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
+};
 
 export default App
