b08b885cefd9ae6542a2dfbee070909b04e921c4
Vamshi <vamshiii2795@gmail.com>
2025-06-17 23:47:52 +0530

    changes done on the title

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
-import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
-import './footer.css';
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import "./footer.css";
 
 const Footer = () => {
   return (
@@ -9,46 +9,83 @@ const Footer = () => {
         <div className="footer-section-container">
           <div className="footer-section-about">
             <h1>P.E.M High School & Jr College</h1>
-            <p>Shaping the future through education and excellence. Join us in building a better tomorrow.</p>
+            <p>
+              Shaping the future through education and excellence. Join us in
+              building a better tomorrow.
+            </p>
             <div className="social-icons">
-              <a href="#"><FaFacebook /></a>
-              <a href="#"><FaInstagram /></a>
-              <a href="#"><FaYoutube /></a>
-              <a href="#"><FaTwitter /></a>
+              <a href="#">
+                <FaFacebook />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+              <a href="#">
+                <FaYoutube />
+              </a>
+              <a href="#">
+                <FaTwitter />
+              </a>
             </div>
           </div>
 
           <div className="footer-section">
             <h3>Quick Links</h3>
             <ul>
-              <li><a href="/SchoolAdmissions">Admissions</a></li>
-              <li><a href="/Toppers">Academics</a></li>
-              <li><a href="/Blog">Events</a></li>
-              <li><a href="#ContactUs">Contact</a></li>
+              <li>
+                <a href="/SchoolAdmissions">Admissions</a>
+              </li>
+              <li>
+                <a href="/Toppers">Academics</a>
+              </li>
+              <li>
+                <a href="/Blog">Events</a>
+              </li>
+              <li>
+                <a href="#ContactUs">Contact</a>
+              </li>
             </ul>
           </div>
 
           <div className="footer-section">
             <h3>Resources</h3>
             <ul>
-              <li><a href="#">Library</a></li>
-              <li><a href="/RightCurriculum">Curriculum</a></li>
-              <li><a href="#">Student Login</a></li>
-              <li><a href="#">Careers</a></li>
+              <li>
+                <a href="#">Library</a>
+              </li>
+              <li>
+                <a href="/RightCurriculum">Curriculum</a>
+              </li>
+              <li>
+                <a href="#">Student Login</a>
+              </li>
+              <li>
+                <a href="#">Careers</a>
+              </li>
             </ul>
           </div>
 
           <div className="footer-section">
             <h3>Newsletter</h3>
             <p>Stay updated with school news and events.</p>
-            <form className="newsletter-form">
-              <input type="email" placeholder="Your Email" />
+            <form
+              action="https://api.web3forms.com/submit"
+              method="POST"
+              className="newsletter-form"
+            >
+              <input
+                type="hidden"
+                name="access_key"
+                value="99a36398-df7f-44fe-a4cd-eeccba85ab44"
+              ></input>
+              <input name="Email" type="email" placeholder="Your Email" />
               <button type="submit">Subscribe</button>
             </form>
           </div>
         </div>
         <div className="footer-bottom">
-          &copy; {new Date().getFullYear()} P.E.M High School & Jr College. All rights reserved.
+          &copy; {new Date().getFullYear()} P.E.M High School & Jr College. All
+          rights reserved.
         </div>
       </div>
     </footer>
